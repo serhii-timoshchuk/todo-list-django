e2714f190cfba957c6dd0585bbfc245e6bb338d3
serhii timoshchuk <thegandalfgreat@gmail.com>
2022-06-05 14:29:06 +0300

    delete not used classes in forms.py

--- todo/forms.py
+++ todo/forms.py
@@ -1,9 +1,7 @@
 from django.contrib.auth import password_validation
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.core import validators
 from django.core.exceptions import ValidationError
-from django.core.validators import EmailValidator
 from django.forms import ModelForm
 from django import forms
 
